e213ddc71306b3d5299b0f036799e2fa0722a0d1
pictdesign <ask@pict.design>
2022-07-25 23:27:31 +0300

    add: logger

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const router = require('./router/index');
+const router = require('./routers/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const PORT = process.env.port || 3000;
+const { PORT = 3000, DATABASE = 'mongodb://localhost:27017/moviedb' } = process.env.port;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/moviedb', {
+mongoose.connect(DATABASE, {
   useNewUrlParser: true,
 });
 
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+app.use(errorLogger);
 
 app.listen(PORT);
