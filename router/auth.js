ab3712d191fb77c1dc74a2d6f072868284e64b25
pictdesign <ask@pict.design>
2022-07-25 16:53:52 +0300

    add: celebrate to user routes

--- router/auth.js
+++ router/auth.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 const { createUser, login, logout } = require('../controllers/users');
 
 const router = express.Router();
 
-router.post('/signin', login);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.sting().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
 
 router.get('/signout', logout);
 
--- router/users.js
+++ router/users.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 const { getMe, updateMe } = require('../controllers/users');
 
 const router = express.Router();
 
 router.get('/me', getMe);
 
-router.patch('/me', updateMe);
+router.patch('/me', celebrate({
+  body: Joi.object.keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), updateMe);
 
 module.exports = router;
