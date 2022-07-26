30669ff7418a508a52afbaafc7a4e0a9de8f55ba
pictdesign <ask@pict.design>
2022-07-26 11:28:07 +0300

    fix: change logs folder

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,16 +1,23 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const fs = require('fs');
+const path = require('path');
+
+const logFolder = 'logs';
+if (!fs.existsSync(logFolder)) {
+  fs.mkdirSync(logFolder);
+}
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: path.join(logFolder, '/request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: path.join(logFolder, '/error.log') }),
   ],
   format: winston.format.json(),
 });
