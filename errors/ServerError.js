118e246dbb88a9de4c57440dfe882a6dffacdcdf
pictdesign <ask@pict.design>
2022-07-27 17:55:43 +0300

    remove: server error

--- errors/ServerError.js
+++
@@ -1,8 +0,0 @@
-class ServerError extends Error {
-  constructor(message = 'Ошибка сервера, попробуйте позже') {
-    super(message);
-    this.statusCode = 500;
-  }
-}
-
-module.exports = ServerError;
